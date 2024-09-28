8388be7a7dc7c2f38fd6cbf1e301cb1a3a342100
Danni Garcia <jodadev.col@gmail.com>
2024-09-29 00:05:34 +1000

    Update tailwind.config.mjs to add new font size and spacing options

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,16 @@ export default {
       fontFamily: {
         sans: ['Red Hat Text Variable', defaultTheme.fontFamily.sans]
       },
+      fontSize: {
+        '5xl': [
+          '2.5rem',
+          {
+            lineHeight: '3.5rem'
+          }
+        ]
+      },
       spacing: {
+        3.5: '0.781rem',
         21: '5.25rem',
         22: '5.5rem'
       },
@@ -24,6 +33,9 @@ export default {
           900: '#260F08'
         },
         green: '#1EA575'
+      },
+      boxShadow: {
+        'inner-rose': 'inset 0 0 1px 1px rgb(173,138,133)'
       }
     }
   },
